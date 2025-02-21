302a403abb7ca729f73841fb264fe87f92b1ba1f
Arif0388 <arifhussai542@gmail.com>
2025-02-21 22:05:55 +0530

    add googleSignIn Button and Email Button and app logo

--- lib/Pages/WelcomePage/WelcomePage.dart
+++ lib/Pages/WelcomePage/WelcomePage.dart
@@ -1,9 +1,10 @@
 import 'package:desi_mart/Config/AssetsPath.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 
-class Welcomepage extends StatelessWidget {
-  const Welcomepage({super.key});
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +14,9 @@ class Welcomepage extends StatelessWidget {
         children: [
           Container(
             padding:const EdgeInsets.all(5),
-            margin:const EdgeInsets.symmetric(vertical:80,horizontal:25),
-            width:335,
-            height:400,
+            margin:const EdgeInsets.symmetric(vertical:50,horizontal:25),
+            // width:335,
+            // height:400,
             decoration:BoxDecoration(
               borderRadius:BorderRadius.circular(20),
               color:Theme.of(context).colorScheme.onSurface,
@@ -25,7 +26,12 @@ class Welcomepage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment:CrossAxisAlignment.start,
                 children: [
-                  SvgPicture.asset(IconsPath.appLogoBanner),
+                  // SvgPicture.asset(IconsPath.appLogoBanner),
+                 Row(children: [
+                   Expanded(flex:0,child: Image.asset(ImagesPath.appLogo,width:80,)),
+                   SizedBox(width:10),
+                   Expanded(child: Text('Welcome To Shopper',style:Theme.of(context).textTheme.labelLarge))
+                 ],),
                  const SizedBox(height:50,),
                  Row(children: [
                    const SizedBox(width:10,),
@@ -78,17 +84,24 @@ class Welcomepage extends StatelessWidget {
           const SizedBox(height:5),
            Text("""Get your grocery in as fast as 
                  one hours""",style:Theme.of(context).textTheme.bodyMedium,),
-            Container(
-            padding:const EdgeInsets.all(5),
-            margin:const EdgeInsets.symmetric(vertical:65),
-            width:335,
-            height:65,
-            decoration:BoxDecoration(
-            borderRadius:BorderRadius.circular(20),
-            color:Theme.of(context).colorScheme.onSurface,
+          SizedBox(height:50),
+          SizedBox(
+            width:280,
+            height:50,
+            child: SignInButton(
+                elevation:5,
+                Buttons.google, onPressed:(){}
             ),
-              child:Center(child: Text('LOGIN ',style:Theme.of(context).textTheme.labelLarge?.copyWith(color:Theme.of(context).colorScheme.primary))),
-            )
+          ),
+          SizedBox(height:20),
+          SizedBox(
+            width:280,
+            height:50,
+            child: SignInButton(
+                elevation:5,
+                Buttons.email, onPressed:(){}
+            ),
+          ),
         ],
       )
     );
