feec60da84c497761bc4d4cca65edb340b207c00
Arif0388 <arifhussai542@gmail.com>
2025-02-25 12:03:31 +0530

    add functionality for user log in or not (SplashController)

--- lib/Pages/Splash_Screen/Splash_Screen.dart
+++ lib/Pages/Splash_Screen/Splash_Screen.dart
@@ -1,7 +1,11 @@
 
+import 'dart:async';
+
 import 'package:desi_mart/Config/AssetsPath.dart';
-import 'package:desi_mart/Controller/authController.dart';
+import 'package:desi_mart/Controller/get_user_data_controller.dart';
+import 'package:desi_mart/Pages/AdminPage/adminPage.dart';
 import 'package:desi_mart/Pages/HomePage/HomePage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -18,30 +22,36 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
 
 late AnimationController controller;
 late Animation<double> animation;
-AuthController authController  = Get.put(AuthController());
+GetUserDataController getUserDataController = Get.put(GetUserDataController());
   @override
   void initState()
   {
     super.initState();
   controller = AnimationController(vsync:this,duration:const Duration(seconds:2))..repeat(reverse:true);
   animation = Tween<double>(begin:0.8,end:1.0).animate(CurvedAnimation(parent: controller, curve:Curves.easeInOut));
-  SplashController();
+   Timer(Duration(seconds:3),(){
+     SplashController();
+   });
   }
 
-  Future<void> SplashController()async{
-    await Future.delayed(const Duration(seconds:3)).then((value){
-     if(authController.auth.currentUser!=null)
-       {
-         Get.offAll(HomePage());
-       }else{
-       Get.offAll(WelcomePage());
-     }
+ Future<void> SplashController()async{
+    User? user = FirebaseAuth.instance.currentUser;
+    if(user!= null)
+      {
+        var userData = await getUserDataController.getUserData(user.uid);
+        if(userData[0]['isAdmin']==true){
+          Get.offAll(AdminPage());
+        }else{
+          Get.offAll(HomePage());
+        }
+      }else{
+      Get.offAll(WelcomePage());
+    }
 
-
-    });
-  }
+ }
 
   void dispose(){
+    controller.dispose();
     super.dispose();
   }
 
