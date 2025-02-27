a7677789ee3cecccd5f05b6459b225e93bd838bc
Arif0388 <arifhussai542@gmail.com>
2025-02-27 20:51:10 +0530

    change list to RxList

--- lib/Controller/banner_controller.dart
+++ lib/Controller/banner_controller.dart
@@ -1,10 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:get/get.dart';
 
 class BannerController extends GetxController
 {
   FirebaseFirestore db = FirebaseFirestore.instance;
-  List<String> banners = [];
+  RxList<String> banners = RxList<String>([]);
 
   @override
   void onInit()
@@ -18,7 +18,7 @@ class BannerController extends GetxController
     try{
       QuerySnapshot bannersSnapshot = await db.collection('banners').get();
       if(bannersSnapshot.docs.isNotEmpty){
-        banners = bannersSnapshot.docs.map((e)=>e['imageUrl']as String).toList();
+        banners.value = bannersSnapshot.docs.map((e)=>e['imageUrl']as String).toList();
       }
     }catch(ex){
       print('Error :$ex');
