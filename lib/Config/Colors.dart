1fc603a1f15e9ba243db26256651afc2cddf9764
Arif0388 <arifhussai542@gmail.com>
2025-02-27 20:50:29 +0530

    add new color like black

--- lib/Config/Colors.dart
+++ lib/Config/Colors.dart
@@ -4,5 +4,6 @@ const lightBgColor = Color(0xffF4F4F4);
 const lightFontColor = Color(0xffF4F4F4);
 const lightPrimaryColor = Color(0xff019934);
 const lightPrimaryContainerColor = Color(0xffFFFFFF);
-const lightOnPrimaryContainerColor = Color(0xff8E8E8E);
+// const lightOnPrimaryContainerColor = Color(0xff8E8E8E);
+const lightOnPrimaryContainerColor = Color(0xff108101D);
 const lightSecondaryColor = Color(0xffF6921E);
